45d79cf4a11f881d6010d1ac780cb027a2c230fe
justadamhopkins <adamhopkins87@gmail.com>
2021-04-08 15:16:51 +0100

    chore: init project boilerplate

---
+++ pages/_app.tsx
@@ -0,0 +1,27 @@
+import { AppProps } from "next/app";
+import { GlobalStyles } from "styles/GlobalStyles";
+import { GlobalFontStyles } from "styles/GlobalFontStyles";
+
+const CustomApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <GlobalStyles />
+      <GlobalFontStyles />
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// CustomApp.getInitialProps = async (appContext: AppContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+
+//   return { ...appProps }
+// }
+
+export default CustomApp;
