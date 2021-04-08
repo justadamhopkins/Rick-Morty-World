55fcd5c06496892c3fbe43c3bd7849aa3de557a5
justadamhopkins <adamhopkins87@gmail.com>
2021-04-09 00:01:47 +0100

    refactor: home template to align with wireframes

--- src/components/_templates/HomeTemplate/HomeTemplate.tsx
+++ src/components/_templates/HomeTemplate/HomeTemplate.tsx
@@ -1,61 +1,31 @@
+import { FC } from "react";
+import {
+  Wrapper,
+  IntroImage,
+  IntroCopy,
+  StyledHomePageWrapper,
+} from "./styles";
 import { Button } from "_atoms/Button/Button";
-import React from "react";
-import styled from "styled-components";
-import { Meta } from "../../_shared/Meta/Meta";
+import { Meta } from "_shared/Meta/Meta";
+import { Hero } from "_organisms/Hero/Hero";
+import { Title } from "_atoms/Title/Title";
+import { Card } from "_molecules/Card/Card";
 
 interface IIntroTemplateProps {
   className?: string;
   title: string;
 }
 
-export const HomeTemplate: React.FC<IIntroTemplateProps> = ({
-  className,
-  title,
-}) => {
+export const HomeTemplate: FC<IIntroTemplateProps> = ({ className, title }) => {
   return (
     <>
       <Meta title={title} />
-      <Wrapper className={className}>
-        <IntroImage src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-
-        <IntroCopy>
-          Please use the <strong>README.md</strong> file located in this repo to
-          start the challenge.
-        </IntroCopy>
-        <IntroCopy>
-          Remember you do not need to complete both parts of this task. See how
-          far you can get.
-        </IntroCopy>
-        <IntroCopy>
-          If you cannot complete it, then please ensure you can let us know what
-          it is you would have done should you have had more time.
-        </IntroCopy>
-        <IntroCopy>** You can delete this file 👍</IntroCopy>
-        <Button />
-      </Wrapper>
+      <StyledHomePageWrapper>
+        <Hero>
+          <Title copy="Rick and Morty" />
+        </Hero>
+        <Card />
+      </StyledHomePageWrapper>
     </>
   );
 };
-
-const Wrapper = styled.div`
-  position: relative;
-  max-width: 600px;
-  margin: 0 auto;
-  text-align: center;
-  padding: 40px;
-`;
-
-const IntroImage = styled.img`
-  width: 200px;
-  height: 200px;
-  border-radius: 50%;
-  margin-bottom: 20px;
-`;
-
-const IntroCopy = styled.p`
-  font-size: 18px;
-  line-height: 1.5;
-  color: white;
-`;
-
-export default IntroTemplate;
