c22968ab6756d05e7a39344a8f5fcc1af742a950
justadamhopkins <adamhopkins87@gmail.com>
2021-04-09 00:00:20 +0100

    refactor: global fonts to use correct name for p tags

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import { breakpoint } from "./BreakpointStyles";
 
 export const GlobalStyles = createGlobalStyle`
   :root {
@@ -15,6 +16,8 @@ export const GlobalStyles = createGlobalStyle`
     --h5: 1.333rem;
     --p: 2rem;
     --small: 0.707rem;
+    --background: var(--materialBlack);
+    --secondaryBackground: var(--darkPink);
   }
   ${reset}
   *, *:before, *:after {
@@ -31,35 +34,53 @@ export const GlobalStyles = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    height: 100%;
+       height: 100vh;
   }
   body {
     font-weight: 400;
     line-height: 1.75;
     color: #000000;
-    background-color: var(--materialBlack)
-  }
-  p {
-    margin-bottom: 1rem;
-    font-family: 'firaCode', Tahoma, Geneva, Verdana, sans-serif;
-    font-size: var(--p);
+    background-color: var(--background);
   }
   h1,h2,h3,h4,h5 {
     font-family: 'merriweather-bold', Tahoma, Geneva, Verdana, sans-serif;
   }
   h1 {
-    font-size: var(--h1)
+    font-size: var(--h2);
+    ${breakpoint.md`
+      font-size: var(--h1);
+  `}
   }
   h2 {
-    font-size: var(--h2)
+    font-size: var(--h3);
+    ${breakpoint.md`
+      font-size: var(--h2);
+  `}
   }
   h3 {
-    font-size: var(--h3)
+    font-size: var(--h4);
+    ${breakpoint.md`
+      font-size: var(--h3);
+  `}
   }
   h4 {
-    font-size: var(--h4)
+    font-size: var(--h5);
+    ${breakpoint.md`
+      font-size: var(--h4);
+  `}
   }
   h5 {
-    font-size: var(--h5)
+    font-size: var(--h5);
+  }
+  p {
+    margin-bottom: 1rem;
+    font-family: 'firaCode-regular', Tahoma, Geneva, Verdana, sans-serif;
+    font-size: var(--p);
+  }
+  ul {
+    list-style-type: none;
+  }
+  #__next {
+    height: 100%;
   }
 `;
