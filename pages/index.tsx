c3e423a6fede3ed8e73143ab9d6674a20d322b62
justadamhopkins <adamhopkins87@gmail.com>
2021-04-09 21:55:37 +0100

    feat: added get server side props to home page to fetch data from the api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,24 @@
 import { NextPage } from "next";
 import { TemplateRenderer } from "_shared/TemplateRenderer/TemplateRenderer";
+import { GetServerSideProps } from "next";
+import { InferGetServerSidePropsType } from "next";
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = ({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <TemplateRenderer
       templateName="home"
-      templateProps={{ title: "Rick and Morty FE Tech Test" }}
+      templateProps={{ title: "Rick and Morty FE Tech Test", data }}
     />
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await fetch("http://localhost:3000/api/characters");
+  const data = await res.json();
+
+  return { props: { data } };
 };
 
 export default HomePage;
