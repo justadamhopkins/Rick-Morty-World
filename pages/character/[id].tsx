b6a4aba99c3e60a06eeb0be0be22f33e49a90be7
justadamhopkins <adamhopkins87@gmail.com>
2021-04-12 21:56:59 +0100

    feat: added character page to consume custom api route

--- pages/character/[id].tsx
+++ pages/character/[id].tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 
 const Character = () => {
@@ -9,6 +10,16 @@ const Character = () => {
       <h1>Post: {id}</h1>
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({
+  query: { id },
+}) => {
+  const res = await fetch(`http://localhost:3000/api/character/${id}`);
+
+  const data = await res.json();
+
+  return { props: { data } };
 };
 
 export default Character;
