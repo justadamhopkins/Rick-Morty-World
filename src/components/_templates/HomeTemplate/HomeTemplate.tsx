031b6a140c1e40f08662ef2e5bf2695d9c5019e3
justadamhopkins <adamhopkins87@gmail.com>
2021-04-12 15:30:51 +0100

    refactor: removed meta from home template

--- src/components/_templates/HomeTemplate/HomeTemplate.tsx
+++ src/components/_templates/HomeTemplate/HomeTemplate.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
 import { StyledHomePageWrapper, StyledSection } from "./styles";
-import { Meta } from "_shared/Meta/Meta";
 import { Hero } from "_organisms/Hero/Hero";
 import { Title } from "_atoms/Title/Title";
 import { Card } from "_molecules/Card/Card";
@@ -10,23 +9,9 @@ interface HomeTemplateProps {
   data: LickApi.ICharacterCore[];
 }
 
-const cardVariant = {
-  open: {
-    y: 0,
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.3,
-      delayChildren: 0.2,
-      staggerDirection: 1,
-      when: "afterChildren",
-    },
-  },
-};
-
 export const HomeTemplate: FC<HomeTemplateProps> = ({ title, data }) => {
   return (
     <>
-      <Meta title={title} />
       <StyledHomePageWrapper>
         <Hero>
           <Title copy="Rick and Morty" />
