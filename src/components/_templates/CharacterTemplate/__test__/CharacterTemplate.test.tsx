e50854602f4ce36fed301415d2545162105c3bf3
justadamhopkins <adamhopkins87@gmail.com>
2021-04-13 22:36:44 +0100

    chore: added more unit tests for character template

--- src/components/_templates/CharacterTemplate/__test__/CharacterTemplate.test.tsx
+++ src/components/_templates/CharacterTemplate/__test__/CharacterTemplate.test.tsx
@@ -3,13 +3,24 @@ import { CharacterTemplate } from "../CharacterTemplate";
 import { getMockCharacterData } from "./mockCharacterData";
 
 describe("CharacterTemplate", () => {
-  const renderCharacterTemplate = () => {
-    return render(<CharacterTemplate data={getMockCharacterData()} />);
+  const renderCharacterTemplate = (data = getMockCharacterData()) => {
+    return render(<CharacterTemplate data={data} />);
   };
   test("should render the CharacterTemplate template", () => {
     renderCharacterTemplate();
 
     expect(screen.getByText("Back to character listing")).toBeTruthy();
     expect(screen.getByText("Rick Sanchez")).toBeTruthy();
+  });
+
+  test("should handle origin being falsy", () => {
+    renderCharacterTemplate({ ...getMockCharacterData(), origin: null });
+
+    expect(screen.getByText("Origin: Unknown")).toBeTruthy();
+  });
+  test("should handle location being falsy", () => {
+    renderCharacterTemplate({ ...getMockCharacterData(), location: null });
+
+    expect(screen.getByText("Location Unknown")).toBeTruthy();
   });
 });
