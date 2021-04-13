43b08bf9946a0bdab9fd920e05c5125355766a0f
justadamhopkins <adamhopkins87@gmail.com>
2021-04-13 13:55:34 +0100

    feat: built out mobile hero for character template

--- src/components/_templates/CharacterTemplate/CharacterTemplate.tsx
+++ src/components/_templates/CharacterTemplate/CharacterTemplate.tsx
@@ -3,17 +3,28 @@ import { Hero } from "_organisms/Hero/Hero";
 import { Title } from "_atoms/Title/Title";
 import ProfileImage from "_atoms/ProfileImage/ProfileImage";
 import { StyledCharacterTemplateWrapper } from "./styles";
+import Link from "next/link";
+import { Arrow } from "_atoms/Arrow/Arrow";
 
 interface CharacterTemplateProps {
   data: LickApi.ICharacter;
 }
 
 export const CharacterTemplate: FC<CharacterTemplateProps> = ({ data }) => {
+  console.log(data);
   return (
     <StyledCharacterTemplateWrapper>
-      <Hero>
+      <Hero fluidHeroHeight={true}>
         <Title copy="Rick and Morty" />
+        <Link href="/">
+          <a>
+            <Arrow direction="left" />
+            Back to character listing
+          </a>
+        </Link>
         <ProfileImage image={data.avatar} alt={data.name} />
+        <h2>{data.name}</h2>
+        <p>Origin: {data.origin.name}</p>
       </Hero>
     </StyledCharacterTemplateWrapper>
   );
--- src/components/_templates/CharacterTemplate/styles.ts
+++ src/components/_templates/CharacterTemplate/styles.ts
@@ -2,11 +2,11 @@ import styled from "styled-components";
 import { StyledHeroWrapper } from "_organisms/Hero/styles";
 
 export const StyledCharacterTemplateWrapper = styled.div`
-  height: 100%;
   ${StyledHeroWrapper} {
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: flex-start;
     align-items: center;
+    padding-top: 20px;
   }
 `;
