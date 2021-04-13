3bad1f30217f94cbaa39c24cde6fd638ea68c827
justadamhopkins <adamhopkins87@gmail.com>
2021-04-13 14:42:50 +0100

    feat: added alias resoulution for jest

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,12 @@ module.exports = {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
+  modulePaths: ["src"],
+  moduleNameMapper: {
+    "^_atoms/(.*)": "<rootDir>/src/components/_atoms/$1",
+    "^_molecules/(.*)": "<rootDir>/src/components/_molecules/$1",
+    "^_organisms/(.*)": "<rootDir>/src/components/_organisms/$1",
+    "^_templates/(.*)": "<rootDir>/src/components/_templates/$1",
+    "^_shared/(.*)": "<rootDir>/src/components/_shared/$1",
+  },
 };
